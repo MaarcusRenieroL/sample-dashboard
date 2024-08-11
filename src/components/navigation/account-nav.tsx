9b3d6dc6948c896d8ca3de09e2eee945d169fd62
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-08-12 00:04:52 +0530

    feat: update account nav

--- src/components/navigation/account-nav.tsx
+++ src/components/navigation/account-nav.tsx
@@ -4,23 +4,35 @@ import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/button";
 import {
   DropdownMenu,
+  DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuPortal,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
-import Link from "next/link";
 import type { FC } from "react";
+import {
+  Archive,
+  ArrowLeftRight,
+  Bot,
+  Code,
+  Gem,
+  Lock,
+  LogOut,
+  Moon,
+  MoreHorizontal,
+  Sun,
+  User,
+  Users,
+} from "lucide-react";
 
-type Props = {
-  name: string;
-  email: string;
-};
-
-export const Account: FC<Props> = ({ name, email }) => {
+export const Account: FC = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -34,30 +46,108 @@ export const Account: FC<Props> = ({ name, email }) => {
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">{name}</p>
-            <p className="text-xs leading-none text-muted-foreground">
-              {email}
-            </p>
-          </div>
-        </DropdownMenuLabel>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           <DropdownMenuItem>
-            Profile
-            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
           </DropdownMenuItem>
-          <Link href="/dashboard/settings">
-            <DropdownMenuItem>Settings</DropdownMenuItem>
-          </Link>
+          <DropdownMenuItem>
+            <Bot className="mr-2 h-4 w-4" />
+            <span>Automations</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Code className="mr-2 h-4 w-4" />
+            <span>Developers</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Archive className="mr-2 h-4 w-4" />
+            <span>Archive</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Lock className="mr-2 h-4 w-4" />
+            <span>Administration</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Users className="mr-2 h-4 w-4" />
+            <span>Teams</span>
+          </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>
+            <ArrowLeftRight className="mr-2 h-4 w-4" />
+            <span>Switch Account</span>
+          </DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+              <DropdownMenuItem>
+                <User className="mr-2 h-4 w-4" />
+                <span>User 1</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <User className="mr-2 h-4 w-4" />
+                <span>User 2</span>
+              </DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>
+            <ArrowLeftRight className="mr-2 h-4 w-4" />
+            <span>Change Theme</span>
+          </DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+              <DropdownMenuItem>
+                <Sun className="mr-2 h-4 w-4" />
+                <span>Light</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Moon className="mr-2 h-4 w-4" />
+                <span>Dark</span>
+              </DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
+        <DropdownMenuSeparator />
         <DropdownMenuItem>
-          Log out
-          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Logout</span>
         </DropdownMenuItem>
+        <DropdownMenuItem className="bg-blue-500 m-2 text-white">
+          <Gem className="mr-2 h-4 w-4" />
+          <span>Upgrade</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <div className="p-2 space-y-3">
+          <p>Work Status</p>
+          <DropdownMenuCheckboxItem>Don't Disturb</DropdownMenuCheckboxItem>
+          <div className="flex items-center justify-evenly">
+            <DropdownMenuCheckboxItem>On</DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem>Off</DropdownMenuCheckboxItem>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                <MoreHorizontal className="mr-2 h-4 w-4" />
+                <span>More</span>
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                  <DropdownMenuItem>
+                    <Sun className="mr-2 h-4 w-4" />
+                    <span>Light</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Moon className="mr-2 h-4 w-4" />
+                    <span>Dark</span>
+                  </DropdownMenuItem>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+          </div>
+        </div>
       </DropdownMenuContent>
     </DropdownMenu>
   );
